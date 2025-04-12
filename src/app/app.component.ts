0c74ec3192d2db1496f2489c2759dc6df32cfd74
Revaldi Dwi Octavian <revaldidwioctaian@gmail.com>
2025-04-12 16:36:38 +0700

    Init project & create navbar

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {LayoutComponent} from './components/layout/layout.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [LayoutComponent],
+  templateUrl: './app.component.html',
+  standalone: true,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'RevDO';
+}
---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'layout',
+  imports: [],
+  templateUrl: './layout.component.html',
+  standalone: true,
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
